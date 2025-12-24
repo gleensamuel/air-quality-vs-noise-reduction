80c7910d67a8de5703564bae6fae0e7ea23bfbd5
Gleen Samuel Andhieka Pradana <pradanaxena@gmail.com>
2025-12-24 18:37:58 +0700

    Update finalproject_visdat.py

--- finalproject_visdat.py
+++ finalproject_visdat.py
@@ -43,6 +43,15 @@ df[num_cols] = df[num_cols].fillna(df[num_cols].median())
 
 tahun_cols = [str(y) for y in range(2025, 2051)]
 
+st.info(
+    "📌 ***Background & Objectives Connecting technical data with the everyday stories of city residents*** 
+    Cities with heavy traffic face two interrelated environmental problems: declining air quality and high road noise. Various low-emission transportation and traffic management policies not only target air pollution reduction, but also have the potential to reduce noise levels in residential areas.
+
+This visualization uses data on air quality and noise indicators measured for each small area and several years of projections. The goal is to show how a single intervention can yield two benefits at once (“co-benefits”), as well as to help readers see spatial and temporal patterns that are not immediately apparent from tables of raw numbers.
+
+In the following sections, the storyline moves from the daily problems experienced by residents, the relationship between the two indicators, areas that improved more quickly, to the dynamics of change over time for a particular area.
+)
+
 # ======================
 # SIDEBAR
 # ======================
